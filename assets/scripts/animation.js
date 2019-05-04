c059580e366b55dd11176d236106dd0208a741bb
Elizaveta716 <sadbadunicorn@gmail.com>
2019-05-05 00:15:15 +0200

    Add skills, CV and contacts block

--- assets/scripts/animation.js
+++ assets/scripts/animation.js
@@ -7,3 +7,13 @@ $(".cat-wink").mouseleave(function () {
     $(this).hide();
     $(".main-img").show();
 });
+
+$(".cat-head").hover(function () {
+    $(this).hide();
+    $(".cat-wink-footer").css("display", "block")
+});
+
+$(".cat-wink-footer").mouseleave(function () {
+    $(this).hide();
+    $(".cat-head").show();
+});
